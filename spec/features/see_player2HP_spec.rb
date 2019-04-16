5e9aae9aadc4d8f2291e5988bad98c6d4f53e782
Chenbo Yi <yichenbo@outlook.com>
2019-04-16 23:00:51 +0100

    see Players's HP

--- spec/features/see_player2HP_spec.rb
+++ spec/features/see_player2HP_spec.rb
@@ -1,12 +1,12 @@
 require 'spec_helper'
 
-feature "Input User names" do
-  scenario "Users inputs their names" do
+feature "See Player2's HP" do
+  scenario "show Player2's HP" do
     visit('/')
     fill_in :player1_name, with: "Name1"
     fill_in :player2_name, with: "Name2"
 
     click_button "Let's Play"
-    expect(page).to have_text('Name1 vs. Name2')
+    expect(page).to have_text('Name2: 60HP')
   end
 end
